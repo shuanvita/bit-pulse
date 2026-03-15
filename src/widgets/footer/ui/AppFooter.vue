d2d2167ba54446290c45139be805253a2e50393b
Andrey Shurygin <shuanvita@gmail.com>
2026-03-15 19:35:56 +0300

    Fix верстки футера

--- src/widgets/footer/ui/AppFooter.vue
+++ src/widgets/footer/ui/AppFooter.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer class="border-primary-border border-t py-6">
+  <footer class="border-primary-border border-t py-6 text-center lg:text-left">
     <div class="container">
-      <span class="text-[#94A3B8] text-[14px] font-medium">© 2026 BitPulse</span>
+      <span class="text-gray-400 text-[14px] font-medium">© 2026 BitPulse</span>
     </div>
   </footer>
 </template>
